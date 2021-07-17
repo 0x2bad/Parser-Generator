abf25bff3674e9fec0fbd6d799a84b8b4c28d60f
Nuclear-Catapult <nuclear.catapult@gmail.com>
2021-07-16 23:23:49 -0500

    'fn goto' now expects and returns full closure instead of kernel

--- main.rs
+++ main.rs
@@ -5,13 +5,14 @@ fn closure(g: &Vec<Vec<Vec<usize>>>, kernel: &Vec<Item>) -> Vec<Item>
 {
     let mut j = kernel.to_vec();
     let mut added = vec![false; g.len()];
-    loop {
+    loop
+    {
         let n = j.len();
         for i in 0..n
         {
             let b = g[j[i].0][j[i].1][j[i].2];
             if added[b] == false
-            {
+            {// item can be added since not found
                 added[b] = true;
                 for p in 0..g[b].len() { j.push(Item(b, p, 0)); }
             }
@@ -23,29 +24,39 @@ fn closure(g: &Vec<Vec<Vec<usize>>>, kernel: &Vec<Item>) -> Vec<Item>
 fn goto(g: &Vec<Vec<Vec<usize>>>, k: &Vec<Item>, t: usize) -> Vec<Item>
 {
     let mut j: Vec<Item> = vec![];
-    let l = closure(g, k);
 
-    for i in l
+    for i in k
     {
-        if g[i.0][i.1][i.2] == t { j.push(i); }
+        if g[i.0][i.1][i.2] == t { j.push(*i); }
     }
 
     for i in &mut j { i.2 += 1; }
 
-    return j;
+    return closure(g, &j);
 }
 
-/*
 fn items(g: &Vec<Vec<Vec<usize>>>)
 {
-    let c = closure(g, Item(258, 0, 0));
+    let mut c = vec![vec![Item(258, 0, 0)]];
     let n = c.len();
-    for i in 0..n
+    loop
     {
-        
+        for i in 0..n
+        {
+            for j in 0..g.len()
+            {
+                let mut r = goto(g, &c[i], j);
+                if ! r.is_empty()
+                {
+                    r.sort_unstable();
+                    if let Err(idx) = c.binary_search(&r) { c.insert(idx, r) }
+                }
+            }
+        }
+        if n == c.len() { return };
     }
 }
-*/
+
 /* Terminals */
 // 40. ( -> terminal
 // 41. ) -> terminal
@@ -74,46 +85,52 @@ fn test_goto()
         let kernel = vec![Item(258, 0, 0)];
 
         // goto(I0, E) -> I1
-        let c = goto(&grammar, &kernel, 259);
+        let c = goto(&grammar, &closure(&grammar, &kernel), 259);
         assert_eq!(c[0], Item(258, 0, 1));
         assert_eq!(c[1], Item(259, 0, 1));
         assert_eq!(2, c.len());
 
         // goto(I0, T) -> I2
-        let c = goto(&grammar, &kernel, 260);
+        let c = goto(&grammar, &closure(&grammar ,&kernel), 260);
         assert_eq!(c[0], Item(259, 1, 1));
         assert_eq!(c[1], Item(260, 0, 1));
         assert_eq!(2, c.len());
 
         // goto(I0, F) -> I3
-        let c = goto(&grammar, &kernel, 261);
+        let c = goto(&grammar, &closure(&grammar, &kernel), 261);
         assert_eq!(c[0], Item(260, 1, 1));
         assert_eq!(1, c.len());
 
         // goto(I0, '(') -> I4
-        let c = goto(&grammar, &kernel, 40);
+        let c = goto(&grammar, &closure(&grammar, &kernel), 40);
         assert_eq!(c[0], Item(261, 0, 1));
-        assert_eq!(1, c.len());
+        assert_eq!(c[1], Item(259, 0, 0));
+        assert_eq!(c[2], Item(259, 1, 0));
+        assert_eq!(c[3], Item(260, 0, 0));
+        assert_eq!(c[4], Item(260, 1, 0));
+        assert_eq!(c[5], Item(261, 0, 0));
+        assert_eq!(c[6], Item(261, 1, 0));
+        assert_eq!(7, c.len());
 
         // goto(I0, i) -> I5
-        let c = goto(&grammar, &kernel, 105);
+        let c = goto(&grammar, &closure(&grammar, &kernel), 105);
         assert_eq!(c[0], Item(261, 1, 1));
         assert_eq!(1, c.len());
 
         // goto(I0, +) -> null
-        let c = goto(&grammar, &kernel, 43);
+        let c = goto(&grammar, &closure(&grammar, &kernel), 43);
         assert_eq!(0, c.len());
 
         // goto(I0, *) -> null
-        let c = goto(&grammar, &kernel, 42);
+        let c = goto(&grammar, &closure(&grammar, &kernel), 42);
         assert_eq!(0, c.len());
 
         // goto(I0, ')') -> null
-        let c = goto(&grammar, &kernel, 41);
+        let c = goto(&grammar, &closure(&grammar, &kernel), 41);
         assert_eq!(0, c.len());
 
         // goto(I0, $) -> null
-        let c = goto(&grammar, &kernel, 257);
+        let c = goto(&grammar, &closure(&grammar, &kernel), 257);
         assert_eq!(0, c.len());
     }
     
@@ -121,9 +138,15 @@ fn test_goto()
         let kernel = vec![Item(258, 0, 1), Item(259, 0, 1)];
 
         // goto(I1, +) -> I6
-        let c = goto(&grammar, &kernel, 43);
+        let c = goto(&grammar, &closure(&grammar, &kernel), 43);
         assert_eq!(c[0], Item(259, 0, 2));
-        assert_eq!(1, c.len());
+        assert_eq!(c[1], Item(260, 0, 0));
+        assert_eq!(c[2], Item(260, 1, 0));
+        assert_eq!(c[3], Item(261, 0, 0));
+        assert_eq!(c[4], Item(261, 1, 0));
+        assert_eq!(5, c.len());
+
+
     }
 }
 
